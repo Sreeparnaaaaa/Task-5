ff49c3a402eba377ce3ee6eae46ef4effee2abd5
Sreeparnaaaaa <sreeparnapodder100@gmail.com>
2025-07-01 19:55:01 +0530

    Add files via upload

---
+++ MySql_Task 5.sql
@@ -0,0 +1,30 @@
+CREATE DATABASE company;
+USE company;
+
+CREATE TABLE Customers (
+    CustomerID INT PRIMARY KEY,
+    CustomerName VARCHAR(100)
+);
+
+CREATE TABLE Orders (
+    OrderID INT PRIMARY KEY,
+    CustomerID INT,
+    Product VARCHAR(100),
+    FOREIGN KEY (CustomerID) REFERENCES Customers(CustomerID)
+);
+
+INSERT INTO Customers (CustomerID, CustomerName) VALUES
+(1, 'Alice'),
+(2, 'Bob'),
+(3, 'Charlie'),
+(4, 'David');
+
+INSERT INTO Orders (OrderID, CustomerID, Product) VALUES
+(101, 1, 'Laptop'),
+(102, 2, 'Tablet'),
+(103, 1, 'Phone'),
+(104, 4, 'Monitor'); 
+
+SELECT Customers.CustomerID, CustomerName, OrderID, Product
+FROM Customers
+INNER JOIN Orders ON Customers.CustomerID = Orders.CustomerID;
